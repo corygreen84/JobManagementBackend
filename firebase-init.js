7384ff2c9459b2e5bab8fec4f5a149d91f5a77b5
Cory Green <corygreen84@hotmail.com>
2019-07-14 14:10:59 -0700

    start of the back end stuff

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDVdNUB8gfvSlJad8iQM7LdRC18GGOkAXI",
+    authDomain: "jobmanagement-be355.firebaseapp.com",
+    databaseURL: "https://jobmanagement-be355.firebaseio.com",
+    projectId: "jobmanagement-be355",
+    storageBucket: "",
+    messagingSenderId: "828776829472",
+    appId: "1:828776829472:web:0b8fa84594d1cdbc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
